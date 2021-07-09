572c5fa015e6aa857466a1a96b739dc25cff622e
Joshua Dietrich <Jdietrich0603@gmail.com>
2021-07-09 17:43:29 -0400

    general organization and quiz preparation

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-const port  = process.env.PORT | 8000
+const port  = process.env.PORT | 8000;
 
 app.listen(port, () => {
-  console.log(`Server starting on port ${port}...`)
-}) 
+  console.log(`Server starting on port ${port}...`);
+}); 
 
 app.get('/', (req, res) =>{
-  res.render('index')
-})
+  res.render('index');
+});
 
 app.get('/results', (req, res) =>{
-  res.render('results')
-})
+  res.render('results');
+});
--- public/work.js
+++ public/work.js
@@ -3,7 +3,13 @@ buttonSubmit.addEventListener('click', () =>{
     console.log('clicked')
     let ageInput = document.getElementById('age')
     console.log(ageInput.value);
-    if (ageInput.value === "1"){
+    if (ageInput.value === "1") //if the input is incorrect
+    {
         alert()
     }
+    else //if the input is correct
+    {
+        window.location = "/quiz";
+    }
+
 })
