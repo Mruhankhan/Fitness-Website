03af9af57ad3328306e6d68adebf3cc3874e8bd9
Mruhankhan <71145148+Mruhankhan@users.noreply.github.com>
2021-07-10 17:21:21 -0700

    Make The Quiz Running

--- public/quiz.js
+++ public/quiz.js
@@ -1,4 +1,86 @@
 let radioOne = document.getElementById('exercise-question-0')
+let radioTwo = document.getElementById('exercise-question-1')
+let radioThree = document.getElementById('exercise-question-2')
+let radioFour = document.getElementById('exercise-question-3')
+let radioFive = document.getElementById('mental-question-0')
+let radioSix = document.getElementById('mental-question-1')
+let radioSeven = document.getElementById('mental-question-2')
+let radioEight = document.getElementById('mental-question-3')
+let radioNine = document.getElementById('alcohol-question-0')
+let radioTen = document.getElementById('alcohol-question-1')
+let radioEleven = document.getElementById('alcohol-question-2')
+let radioTwelve = document.getElementById('alcohol-question-3')
+let radioThridteen = document.getElementById('smoke-question-0')
+let radioFourteen = document.getElementById('smoke-question-1')
+let radioFifteen = document.getElementById('smoke-question-2')
+let radioSixteen = document.getElementById('social-question-0')
+let radioSeventeen = document.getElementById('social-question-1')
+let radioEighteen = document.getElementById('social-question-2')
+let radionNineteen = document.getElementById('social-question-3')
+let radioTwenty = document.getElementById('social-question-4')
+let radioTwentyOne = document.getElementById('social-question-5')
+
+
 radioOne.addEventListener('click', () =>{
-    alert()
+    alert("Start Doing So")
+})
+radioTwo.addEventListener('click', () =>{
+    alert("Start Doing More")
+})
+radioThree.addEventListener('click', () =>{
+    alert("Little More Will be better ")
+})
+radioFour.addEventListener('click', () =>{
+    alert("Nice")
+})
+radioFive.addEventListener('click', () =>{
+    alert("Start It!")
+})
+radioSix.addEventListener('click', () =>{
+    alert("Do more")
+})
+radioSeven.addEventListener('click', () =>{
+    alert("Just a little more will be better")
+})
+radioEight.addEventListener('click', () =>{
+    alert("Super Keep Going!")
+})
+radioNine.addEventListener('click', () =>{
+    alert("Nice")
+})
+radioTen.addEventListener('click', () =>{
+    alert("STOP drinking It")
+})
+radioEleven.addEventListener('click', () =>{  
+    alert("STOP drinking it")
+})
+radioTwelve.addEventListener('click', () =>{
+    alert("STOP drinking it")
+})
+radioThridteen.addEventListener('click', () =>{
+    alert("Nice")
+})
+radioFourteen.addEventListener('click', () =>{
+    alert("STOP Its Bad")
+})
+radioFifteen.addEventListener('click', () =>{
+    alert("STOP Its Bad")
+})
+radioSixteen.addEventListener('click', () =>{
+    alert("Nice")
+})
+radioSeventeen.addEventListener('click', () =>{
+    alert("Its Ok Amount")
+})
+radioEighteen.addEventListener('click', () =>{
+    alert("Its Fine But Start More")
+})
+radionNineteen.addEventListener('click', () =>{
+    alert("Start Going More")
+})
+radioTwenty.addEventListener('click', () =>{
+    alert("Start Going More And More")
+})
+radioTwentyOne.addEventListener('click', () =>{
+    alert("Start going alot!")
 })
